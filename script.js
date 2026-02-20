c1c3533070ddccc90e34e09ec825be6c1705ce63
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-20 16:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
 
+let container = document.getElementById("infi-list");
+let count = 1;
+for(let i=0; i<=9; i++){
+	let li = document.createElement("li");
+	li.innerText = "Item " + count++;
+	container.appendChild(li);
+	
+}
+
+container.addEventListener("scroll", function(){
+	if(container.scrollTop + container.clientHeight >= container.scrollHeight){
+		for(let i=0; i<2; i++){
+			let li =  document.createElement("li");
+			li.innerText = "Item " + count++;
+			container.appendChild(li);
+		}
+	}
+})
